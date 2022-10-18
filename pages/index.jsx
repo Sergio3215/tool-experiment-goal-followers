976e157dfb4b6aed15f1e1b11b08b5419fb20df6
Sergio <principiante.en.programar@gmail.com>
2022-10-17 23:09:24 -0300

    Add the expectation about the goal

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 export default function AppIG(){
 
+    
     return (
         <>
-        <iframe src={`https://www.instagram.com/alguien_mas_de_mas/?hl=en`} width="100%" height="800px"/>
+        {/* <iframe src={`https://www.instagram.com/alguien_mas_de_mas/?hl=en`} width="100%" height="800px"/> */}
+        Currently 0 - Expected 10k
         </>
     )
 }
